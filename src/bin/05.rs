46105740438ff483f164d59e0b00ed1f829a63f8
Andrew Baptist <abaptist@ocient.com>
2021-12-05 23:39:51 -0500

    Use the parse-display macro

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,10 +1,14 @@
-use std::cmp::max;
+use parse_display::{Display, FromStr};
 
-//type Point = (usize, usize);
+#[derive(Display, FromStr, Debug)]
+#[display("{start} -> {end}")]
 struct Line {
     start: Point,
     end: Point,
 }
+
+#[derive(Display, FromStr, Debug)]
+#[display("{x},{y}")]
 struct Point {
     x: usize,
     y: usize,
@@ -12,35 +16,17 @@ struct Point {
 const MAX: usize = 1000;
 fn main() {
     let lines = include_str!("../input/5.txt").lines();
-    let lines: Vec<Line> = lines
-        .map(|line| {
-            line.split_once(" -> ")
-                .map(|(p1, p2)| Line {
-                    start: parse_point(p1),
-                    end: parse_point(p2),
-                })
-                .unwrap()
-        })
-        .collect();
-
-    fn parse_point(p: &str) -> Point {
-        p.split_once(',')
-            .map(|(x, y)| Point {
-                x: x.parse().unwrap(),
-                y: y.parse().unwrap(),
-            })
-            .unwrap()
-    }
+    let lines: Vec<Line> = lines.map(|line| line.parse().unwrap()).collect();
 
     let mut board = vec![vec![0; MAX]; MAX];
     for line in lines {
-        let x_diff = line.end.x as i32 - line.start.x as i32;
-        let y_diff = line.end.y as i32 - line.start.y as i32;
+        let x_diff = line.end.x as isize - line.start.x as isize;
+        let y_diff = line.end.y as isize - line.start.y as isize;
 
         if x_diff == 0 || y_diff == 0 || x_diff.abs() == y_diff.abs() {
-            for count in 0..=max(x_diff.abs(), y_diff.abs()) {
-                board[(line.start.x as i32 + count * x_diff.signum()) as usize]
-                    [(line.start.y as i32 + count * y_diff.signum()) as usize] += 1;
+            for count in 0..=x_diff.abs().max(y_diff.abs()) {
+                board[(line.start.x as isize + count * x_diff.signum()) as usize]
+                    [(line.start.y as isize + count * y_diff.signum()) as usize] += 1;
             }
         }
     }
