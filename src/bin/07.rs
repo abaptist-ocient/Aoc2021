8e9702c2ed3848226e530a7f78d2bd2975589c48
Andrew Baptist <abaptist@ocient.com>
2021-12-07 11:42:01 -0500

    Don't bother sorting the list

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,11 +1,10 @@
 fn main() {
-    let mut input: Vec<isize> = include_str!("../input/7.txt")
+    let input: Vec<isize> = include_str!("../input/7.txt")
         .split(',')
         .filter_map(|x| x.trim().parse::<isize>().ok())
         .collect();
-    input.sort_unstable();
-    let output =
-        (*input.first().unwrap()..=*input.last().unwrap()).map(|x| compute_distance(x, &input));
+    let output = (*input.iter().min().unwrap()..=*input.iter().max().unwrap())
+        .map(|x| compute_distance(x, &input));
     println!("{:?}", output.min().unwrap());
 }
 fn compute_distance(point: isize, input: &[isize]) -> isize {
