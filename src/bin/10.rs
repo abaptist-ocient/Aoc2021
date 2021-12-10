544a684fb3589e0d6106b4b736cbcf06edba82f3
Andrew Baptist <abaptist@ocient.com>
2021-12-10 10:55:42 -0500

    golf the code a little

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,50 +1,27 @@
-use itertools::FoldWhile::{Continue, Done};
-use itertools::Itertools;
-
-fn get_value(c: char) -> usize {
-    match c {
-        '(' => 1,
-        '[' => 2,
-        '{' => 3,
-        '<' => 4,
-        ')' => 3,
-        ']' => 57,
-        '}' => 1197,
-        '>' => 25137,
-        _ => 0,
-    }
-}
-
-fn get_close(c: char) -> char {
-    match c {
-        '(' => ')',
-        '[' => ']',
-        '{' => '}',
-        '<' => '>',
-        _ => 'X',
-    }
-}
+use std::collections::HashMap;
 fn main() {
+    let p1: HashMap<char, usize> = HashMap::from([(')', 3), (']', 57), ('}', 1197), ('>', 25137)]);
+    let p2: HashMap<char, usize> = HashMap::from([('(', 1), ('[', 2), ('{', 3), ('<', 4)]);
+    let end: HashMap<char, char> = HashMap::from([('(', ')'), ('[', ']'), ('{', '}'), ('<', '>')]);
+
     let res: Vec<_> = include_str!("../input/10.txt")
         .lines()
         .map(|line| {
-            match line.chars().fold_while(Vec::new(), |mut stack, c| {
+            match line.chars().try_fold(Vec::new(), |mut stack, c| {
                 if matches!(c, '(' | '[' | '{' | '<') {
                     stack.push(c);
-                    Continue(stack)
-                } else if c == get_close(*stack.last().unwrap()) {
+                    Ok(stack)
+                } else if c == end[stack.last().unwrap()] {
                     stack.pop();
-                    Continue(stack)
+                    Ok(stack)
                 } else {
-                    Done(vec![c])
+                    Err(vec![c])
                 }
             }) {
-                // we didn't hit any errors - but there might be open chars we need to count
-                Continue(stack) => {
-                    Continue(stack.iter().rev().fold(0, |acc, c| 5 * acc + get_value(*c)))
-                }
-                // we hit an error - single value stack with the error
-                Done(stack) => Done(get_value(stack[0])),
+                // part 2 we didn't hit any errors - but there might be open chars we need to count
+                Ok(stack) => Ok(stack.iter().rev().fold(0, |acc, c| 5 * acc + p2[c])),
+                // part 1 we hit an error - single value stack with the error
+                Err(stack) => Err(p1[&stack[0]]),
             }
         })
         .collect();
@@ -52,19 +29,12 @@ fn main() {
     println!(
         "Part 1 {}",
         res.iter()
-            .filter_map(|val| if let Done(v) = val { Some(v) } else { None })
+            .filter_map(|val| if let Err(v) = val { Some(v) } else { None })
             .sum::<usize>()
     );
-    let res = res
-        .iter()
-        .filter_map(|val| {
-            if let Continue(v) = val {
-                Some(*v)
-            } else {
-                None
-            }
-        })
-        .sorted()
-        .collect_vec();
+
+    // only keep the success
+    let mut res: Vec<_> = res.iter().filter_map(|val| val.ok()).collect();
+    res.sort_unstable();
     println!("Part 2 {}", res[res.len() / 2]);
 }
