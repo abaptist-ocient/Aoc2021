5dda3a1d25a788adddbab52e9703cf14b20fc753
Andrew Baptist <abaptist@ocient.com>
2021-12-11 17:17:45 -0500

    CHange to a fold just to see if it looks cleaner

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -16,30 +16,29 @@ fn main() {
                 board[i][j] += 1;
             });
         });
-        let mut has_change = true;
-        while has_change {
-            has_change = false;
-            (1..&board.len() - 1).for_each(|i| {
-                (1..board[i].len() - 1).for_each(|j| {
-                    if board[i][j] > 9 {
-                        for x in 0..=2 {
-                            for y in 0..=2 {
-                                let pos = &mut board[i + x - 1][j + y - 1];
-                                if *pos > 0 {
-                                    *pos += 1;
-                                    has_change = true;
-                                }
+        while (1..&board.len() - 1).fold(false, |acc, i| {
+            acc | (1..board[i].len() - 1).fold(false, |acc, j| {
+                acc | if board[i][j] > 9 {
+                    (0..=2).for_each(|x| {
+                        (0..=2).for_each(|y| {
+                            let pos = &mut board[i + x - 1][j + y - 1];
+                            if *pos > 0 {
+                                *pos += 1;
                             }
-                        }
-                        if iter < 100 {
-                            count_flash += 1;
-                        }
-                        board[i][j] = 0;
-                    }
-                });
-            });
-        }
+                        });
+                    });
+                    count_flash += 1;
+                    board[i][j] = 0;
+                    true
+                } else {
+                    false
+                }
+            })
+        }) {}
         iter += 1;
+        if iter == 100 {
+            println!("part 1 {}", count_flash);
+        }
         if (0..&board.len() - 1).fold(true, |acc, i| {
             acc & (0..&board[i].len() - 1).fold(true, |acc, j| acc & (board[i][j] == 0))
         }) {
@@ -47,5 +46,4 @@ fn main() {
             break;
         }
     }
-    println!("part 1 {}", count_flash);
 }
