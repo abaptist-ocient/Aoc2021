1ac0b4b231d66f81c071cce91bbd643f271ec659
Andrew Baptist <abaptist@ocient.com>
2021-12-13 14:58:43 -0500

    Really short solution using points instead of a grid

---
+++ src/bin/13alt.rs
@@ -0,0 +1,25 @@
+fn uint(s: &str) -> usize {
+    s.parse::<usize>().unwrap()
+}
+
+fn main() {
+    if let Some((points, folds)) = include_str!("../input/13.txt").split_once("\n\n") {
+        let mut points: Vec<_> = points .lines() .map(|p| p.split_once(',').map(|(x, y)| (uint(y), uint(x))).unwrap()) .collect();
+        let folds: Vec<_> = folds .lines() .map(|f| { f.split_once('=') .map(|(d, v)| (d.ends_with('x'), uint(v))) .unwrap() }) .collect();
+
+        for fold in folds {
+            for (x, y) in &mut points {
+                let coord = if fold.0 { y } else { x };
+                if *coord > fold.1 {
+                    *coord = 2 * fold.1 - *coord;
+                }
+            }
+        }
+        (0..=points.iter().fold(0, |b, &p| std::cmp::max(b, p.0))).for_each(|i| {
+            (0..=points.iter().fold(0, |b, &p| std::cmp::max(b, p.1))).for_each(|j| {
+                print!("{}", if points.contains(&(i, j)) { '#' } else { ' ' });
+            });
+            println!();
+        });
+    }
+}
