36a3bcfab55aea3c83bdf255263a9a6a9a07b978
Andrew Baptist <abaptist@ocient.com>
2021-12-14 10:48:41 -0500

    Remove duplicates (allows larger grids, but not really necessary)

--- src/bin/13alt.rs
+++ src/bin/13alt.rs
@@ -1,19 +1,37 @@
 #![feature(int_abs_diff)]
 
+use std::collections::HashSet;
+
 fn uint(s: &str) -> usize {
     s.parse::<usize>().unwrap()
 }
 
 fn main() {
-    if let Some((points, folds)) = include_str!("../input/13.txt").split_once("\n\n") {
-        let mut points: Vec<_> = points .lines() .map(|p| p.split_once(',').map(|(x, y)| (uint(y), uint(x))).unwrap()) .collect();
-        let folds: Vec<_> = folds .lines() .map(|f| { f.split_once('=') .map(|(d, v)| (d.ends_with('x'), uint(v))) .unwrap() }) .collect();
+    if let Some((points, folds)) = include_str!("../input/test.txt").split_once("\n\n") {
+        let mut points: Vec<_> = points
+            .lines()
+            .map(|p| p.split_once(',').map(|(x, y)| (uint(y), uint(x))).unwrap())
+            .collect();
+        let folds: Vec<_> = folds
+            .lines()
+            .map(|f| {
+                f.split_once('=')
+                    .map(|(d, v)| (d.ends_with('x'), uint(v)))
+                    .unwrap()
+            })
+            .collect();
 
         for fold in folds {
             for (x, y) in &mut points {
                 let coord = if fold.0 { y } else { x };
                 *coord = fold.1 - coord.abs_diff(fold.1);
             }
+            // remove dupes
+            points = points
+                .into_iter()
+                .collect::<HashSet<_>>()
+                .into_iter()
+                .collect::<Vec<_>>();
         }
         (0..=points.iter().fold(0, |b, &p| std::cmp::max(b, p.0))).for_each(|i| {
             (0..=points.iter().fold(0, |b, &p| std::cmp::max(b, p.1))).for_each(|j| {
