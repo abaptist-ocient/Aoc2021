353d9b973676e706451fe73ceb02d39453ff1d97
Andrew Baptist <abaptist@ocient.com>
2021-12-15 12:37:44 -0500

    Minor cleanups

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -1,43 +1,36 @@
-use itertools::Itertools;
-use std::{cmp::Ordering, collections::BinaryHeap};
+type Position = (usize, usize);
+type Val = (usize, usize);
+const REPEAT: usize = 5;
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq, Default)]
 struct State {
     cost: usize,
-    position: (usize, usize),
+    position: Position,
 }
-
 impl Ord for State {
-    fn cmp(&self, other: &Self) -> Ordering {
-        other
-            .cost
-            .cmp(&self.cost)
-            .then_with(|| self.position.cmp(&other.position))
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        other.cost.cmp(&self.cost)
     }
 }
 impl PartialOrd for State {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         Some(self.cmp(other))
     }
 }
 
-const REPEAT: usize = 5;
-
 fn main() {
-    let lines = include_str!("../input/15.txt")
+    let lines: Vec<Vec<Val>> = include_str!("../input/15.txt")
         .lines()
         .map(|line| {
             line.chars()
                 .map(|c| (c.to_digit(10).unwrap() as usize, usize::MAX))
-                .collect_vec()
+                .collect()
         })
-        .collect_vec();
-
-    let mut grid: Vec<Vec<(usize, usize)>> =
-        vec![vec![(0, usize::MAX); lines.len() * REPEAT]; lines.len() * REPEAT];
+        .collect();
 
     let max_i = lines.len();
     let max_j = lines[0].len();
+    let mut grid: Vec<Vec<Val>> = vec![vec![(0, usize::MAX); max_i * REPEAT]; max_j * REPEAT];
 
     (0..max_i * REPEAT).for_each(|i| {
         (0..max_j * REPEAT).for_each(|j| {
@@ -47,16 +40,13 @@ fn main() {
             )
         })
     });
-    let mut heap = BinaryHeap::<State>::new();
-
-    heap.push(State {
-        cost: 0,
-        position: (0, 0),
-    });
+    grid[0][0].1 = 0;
+    let mut heap = std::collections::BinaryHeap::<State>::new();
+    heap.push(Default::default());
 
     while let Some(State { cost, position }) = heap.pop() {
         if cost <= grid[position.0][position.1].1 {
-            for (i, j) in get_neighbors(position, &grid) {
+            for (i, j) in get_neighbors(position, (grid[0].len(), grid.len())) {
                 let (edge, old_cost) = grid[i][j];
                 let next = State {
                     cost: cost + edge,
@@ -72,7 +62,7 @@ fn main() {
     println!("{}", grid.last().unwrap().last().unwrap().1);
 }
 
-fn get_neighbors((i, j): (usize, usize), grid: &[Vec<(usize, usize)>]) -> Vec<(usize, usize)> {
+fn get_neighbors((i, j): Position, (x, y): Position) -> Vec<Position> {
     let mut neighbors = Vec::new();
     if i > 0 {
         neighbors.push((i - 1, j));
@@ -80,10 +70,10 @@ fn get_neighbors((i, j): (usize, usize), grid: &[Vec<(usize, usize)>]) -> Vec<(u
     if j > 0 {
         neighbors.push((i, j - 1));
     }
-    if i + 1 < grid[0].len() {
+    if i + 1 < x {
         neighbors.push((i + 1, j));
     }
-    if j + 1 < grid.len() {
+    if j + 1 < y {
         neighbors.push((i, j + 1));
     }
     neighbors
